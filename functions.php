c09e0be08c78369ee28c71bebd1f4e96ae165fd0
Nikky Southerland <nikky.southerland@gmail.com>
2012-10-04 16:06:41 -0700

    Additional tweaks and additions.
    * Added next/previous links for the home page and categories
    * Added next/previous links for posts
    * Changed the_excerpt to the_content for index.php (presumably authors can decide what content they want to display)
    * Removed blog without a primary menu from inheriting menu from main blogs network

--- functions.php
+++ functions.php
@@ -1,54 +1,42 @@
 <?  
 function uw_setup()
-  {
+{
 
-          add_theme_support( 'automatic-feed-links' );
-          add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'automatic-feed-links' );
+  add_theme_support( 'post-thumbnails' );
 
-    add_image_size( 'Thimble', 50, 50, true );
-    add_image_size( 'Sidebar', 250, 9999, false );
-    add_image_size( 'Body Image', 300, 9999, false );
-    add_image_size( 'Full Width', 620, 9999, false );
+  add_image_size( 'Thimble', 50, 50, true );
+  add_image_size( 'Sidebar', 250, 9999, false );
+  add_image_size( 'Body Image', 300, 9999, false );
+  add_image_size( 'Full Width', 620, 9999, false );
 
-          register_nav_menu( 'primary', __( 'Primary Menu', 'uw' ) );
-          register_nav_menu( 'footer', __( 'Footer Menu', 'uw' ) );
+  register_nav_menu( 'primary', __( 'Primary Menu', 'uw' ) );
+  register_nav_menu( 'footer', __( 'Footer Menu', 'uw' ) );
 
-    define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1280 ) );
-    define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 215 ) );
+  define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1280 ) );
+  define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 215 ) );
 
-          add_theme_support( 'custom-header', array( 'random-default' => true ) );
+  add_theme_support( 'custom-header', array( 'random-default' => true ) );
           //add_custom_image_header( 'uw_header_style', 'uw_admin_header_style', 'uw_admin_header_image' );
 
-    register_default_headers( array(
-      'blossoms' => array(
-        'url' => '%s/../UW-Wordpress-Theme/img/header/cherries.jpg',
-        'thumbnail_url' => '%s/../UW-Wordpress-Theme/img/header/cherries-thumbnail.jpg',
-        'description' => __( 'Cherry Blossoms', 'uw' )
+  register_default_headers( array(
+    'blossoms' => array(
+      'url' => '%s/../UW-Wordpress-Theme/img/header/cherries.jpg',
+      'thumbnail_url' => '%s/../UW-Wordpress-Theme/img/header/cherries-thumbnail.jpg',
+      'description' => __( 'Cherry Blossoms', 'uw' )
       )
     ));
 };
 
 
-function uw_dropdowns()
+function uw_dropdowns() 
   {
-    $nav = has_nav_menu('primary');
-    if ( !$nav )
-    {
-      //switch_to_blog(1);
-    }
-    wp_nav_menu( array(
+    wp_nav_menu( array( 
       'theme_location'  => 'primary',
       'container_class' => 'nav-collapse',
       'menu_class'      => 'nav',
-      'fallback_cb'     => '',
+      'fallback_cb'     => 'wp_page_menu',
       'walker'          => new UW_Dropdowns_Walker_Menu(),
     ) );
-    if ( !$nav )
-    {
-      //restore_current_blog();
-    }
   }
-
-
-
 ?>
--- index.php
+++ index.php
@@ -6,22 +6,21 @@
 						
 			<div class="row show-grid">
 				<div class="span8">
-				<span id="arrow-mark" <?php the_blogroll_banner_style(); ?> ></span>
-								
+					<span id="arrow-mark"></span>
+					
+					
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php apply_filters('italics', get_the_title()); ?></a></h2>
-					<span class="post-info"><p><?php the_time('F j, Y'); ?></p><p class="author-info">By <?php the_author(); ?></p></span>
+					<h1 class="entry-title"><?php apply_filters('italics', get_the_title()); ?></h1>
 				</header><!-- .entry-header -->
 			
 				<div class="entry-content">
 					<?php the_content(); ?>
 					<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 				</div><!-- .entry-content -->
-				<hr>
 				<footer class="entry-meta">
 					<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 				</footer><!-- .entry-meta -->
@@ -30,12 +29,13 @@
 					<?php comments_template( '', true ); ?>
 
 			<?php endwhile; // end of the loop. ?>
-<?php posts_nav_link(); ?>
-				</div>
-				<div id="secondary" class="span4 right-bar" role="complementary">
+
+				</div
+>				<div id="secondary" class="span4 right-bar" role="complementary">
 					<div class="stripe-top"></div><div class="stripe-bottom"></div>				
           <div id="sidebar">
-          <?php if (is_active_sidebar('homepage-sidebar')) : dynamic_sidebar('homepage-sidebar'); else: dynamic_sidebar('sidebar'); endif; ?>
+          <?php if (is_active_sidebar('homepage-sidebar') && is_front_page() ||
+                    is_active_sidebar('homepage-sidebar') && is_home() ) : dynamic_sidebar('homepage-sidebar'); else: dynamic_sidebar('sidebar'); endif; ?>
           </div>
 				</div>
  			 </div>
